470f3c9c03defe6561faa64b66e8b926278d14a4
leolabtec <kaivvi6@gmail.com>
2025-04-25 18:49:04 +0800

    Update halo.go

--- halo.go
+++ halo.go
@@ -1,49 +1,14 @@
-// halo.go
-package main
+package halo
 
 import (
 	"fmt"
-	"os"
-	"os/exec"
+	"autodeploy-go/dockerops"
 )
 
-// 部署 Halo 博客
-func deployHalo(domain string) error {
-	// 创建 Docker Compose 文件
-	composeFile := fmt.Sprintf("/home/dockerdata/%s/docker-compose.yml", domain)
-	file, err := os.Create(composeFile)
-	if err != nil {
-		return fmt.Errorf("创建 Docker Compose 文件失败: %v", err)
-	}
-	defer file.Close()
-
-	// 写入 Halo 配置
-	composeContent := fmt.Sprintf(`
-version: '3'
-services:
-  halo:
-    image: halohub/halo:2.15
-    ports:
-      - "8081:8080"
-    environment:
-      HALO_PORT: 8080
-    networks:
-      - halo_network
-networks:
-  halo_network:
-    driver: bridge
-`)
-	_, err = file.WriteString(composeContent)
-	if err != nil {
-		return fmt.Errorf("写入 Docker Compose 文件失败: %v", err)
-	}
-
-	// 启动 Halo 容器
-	err = startDockerContainer("halo", "halohub/halo:2.15", "8081:8080")
-	if err != nil {
-		return fmt.Errorf("启动 Halo 容器失败: %v", err)
-	}
-
-	fmt.Printf("Halo 博客站点部署完成: http://%s:8081\n", domain)
-	return nil
+// 创建 Halo 博客
+func CreateHaloSite() {
+	// 创建 Halo 博客所需的容器
+	fmt.Println("[+] 创建 Halo 博客...")
+	// 启动 Docker 容器
+	dockerops.StartContainer("halo", "8090:80", "halohub/halo:latest")
 }
